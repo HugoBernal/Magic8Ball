1afa35391c46a54c5bfedd60b2f73101a5fd335f
Hugo Bernal <hugonobernal@gmail.com>
2018-01-30 22:19:42 -0500

    Added a function for generating a random image between the 5 different images available and implemented the function for when the app starts (viewDidLoad) and when the button ask is touched.

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber = 0
     
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        newBallImage()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +28,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int(arc4random_uniform(4))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
     }
     
 
